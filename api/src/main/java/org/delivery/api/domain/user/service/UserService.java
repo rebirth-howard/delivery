fd7d3f9b916889336f6c2b9877d5c7ecccfd170a
hyunwoo.park <rebirth.howard@gmail.com>
2024-10-03 02:03:51 +0900

    사용자 서비스 로직 - 로그인 샘플

--- api/src/main/java/org/delivery/api/domain/user/service/UserService.java
+++ api/src/main/java/org/delivery/api/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package org.delivery.api.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.user.UserEntity;
 import org.delivery.db.user.UserRepository;
@@ -20,6 +21,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+
     public UserEntity register(UserEntity userEntity){
         return Optional.ofNullable(userEntity)
                 .map(it ->{
@@ -30,4 +32,31 @@ public class UserService {
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "User Entity Null"));
     }
 
+    public UserEntity login(
+            String email,
+            String password
+    ){
+        var entity = getUserWithThrow(email, password);
+        return entity;
+    }
+
+    public UserEntity getUserWithThrow(
+            String email,
+            String password
+    ){
+        return userRepository.findFirstByEmailAndPasswordAndStatusOrderByIdDesc(
+                email,
+                password,
+                UserStatus.REGISTERED
+        ).orElseThrow(()-> new ApiException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    public UserEntity getUserWithThrow(
+            Long userId
+    ){
+        return userRepository.findFirstByIdAndStatusOrderByIdDesc(
+                userId,
+                UserStatus.REGISTERED
+        ).orElseThrow(()-> new ApiException(UserErrorCode.USER_NOT_FOUND));
+    }
 }
